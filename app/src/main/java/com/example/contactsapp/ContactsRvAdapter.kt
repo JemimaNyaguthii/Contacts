1db86fa7e2ba09b399b414e568539663a2f0f3dd
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-06-14 16:55:46 +0300

    AddContact

--- app/src/main/java/com/example/contactsapp/ContactsRvAdapter.kt
+++ app/src/main/java/com/example/contactsapp/ContactsRvAdapter.kt
@@ -5,29 +5,34 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.contactsapp.databinding.ContactListItemBinding
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
 
 class ContactsRvAdapter(var contactData: List<ContactData>):RecyclerView.Adapter<ContactsRvAdapter.ContactsViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
         val binding =ContactListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ContactsViewHolder(binding)
-
+    }
+    override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
+        val currentContact = contactData.get(position)
+        val binding=holder.binding
+        binding.ivAvatar.tag =currentContact.image
+        binding.tvEmail.text=currentContact.email
+        binding.tvName.text=currentContact.name
+        binding.tvPhoneNumber.text =currentContact.phoneNumber
+        Picasso
+            .get().load(currentContact.image)
+            .resize(80 ,80)
+            .centerInside()
+            .transform(CropCircleTransformation())
+            .into(binding.ivAvatar)
     }
 
-
-  override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
-   val currentContact = contactData.get(position)
-      val binding=holder.binding
-      binding.ivAvatar.tag =currentContact.image
-      binding.tvEmail.text=currentContact.email
-      binding.tvName.text=currentContact.name
-      binding.tvPhoneNumber.text =currentContact.phoneNumber
-
-  }
     override fun getItemCount(): Int {
-        return  contactData.size
+        return contactData.size
     }
 
-class ContactsViewHolder(var binding: ContactListItemBinding):RecyclerView.ViewHolder(binding.root)
+    class ContactsViewHolder(var binding: ContactListItemBinding):RecyclerView.ViewHolder(binding.root)
 
 }
 
